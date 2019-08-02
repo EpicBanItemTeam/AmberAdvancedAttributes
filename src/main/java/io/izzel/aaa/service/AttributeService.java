d798822c8aa1edbb571182ee4eb5a19c913c2ec8
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2019-08-03 00:23:56 +0800

    Add static instance getter for AttributeService

--- src/main/java/io/izzel/aaa/service/AttributeService.java
+++ src/main/java/io/izzel/aaa/service/AttributeService.java
@@ -1,5 +1,6 @@
 package io.izzel.aaa.service;
 
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.data.DataSerializable;
 import org.spongepowered.api.util.annotation.NonnullByDefault;
 
@@ -11,6 +12,11 @@ import java.util.Optional;
  */
 @NonnullByDefault
 public interface AttributeService {
+
+    static AttributeService instance() {
+        return Sponge.getServiceManager().provideUnchecked(AttributeService.class);
+    }
+
     Map<String, Attribute<?>> getAttributes();
 
     @SuppressWarnings("unchecked")
