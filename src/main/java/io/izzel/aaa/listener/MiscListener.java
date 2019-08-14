e0a20bd55646bc5ff2dda1ceeae6d7f90db32948
IzzelAliz <csh2001331@126.com>
2019-08-14 15:38:38 +0800

    Implement LIFE_STEAL/KNOCKBACK

--- src/main/java/io/izzel/aaa/listener/MiscListener.java
+++ src/main/java/io/izzel/aaa/listener/MiscListener.java
@@ -8,6 +8,7 @@ import org.spongepowered.api.entity.Entity;
 import org.spongepowered.api.entity.Equipable;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.cause.entity.damage.source.EntityDamageSource;
+import org.spongepowered.api.event.entity.AttackEntityEvent;
 import org.spongepowered.api.event.entity.DamageEntityEvent;
 import org.spongepowered.api.event.filter.Getter;
 import org.spongepowered.api.event.filter.cause.First;
@@ -20,7 +21,7 @@ public class MiscListener {
     private final Random random = new Random();
 
     @Listener
-    public void on(DamageEntityEvent event, @Getter("getTargetEntity") Entity to, @First EntityDamageSource source) {
+    public void onBurn(DamageEntityEvent event, @Getter("getTargetEntity") Entity to, @First EntityDamageSource source) {
         Entity from = source.getSource();
         if (from instanceof Equipable) {
             double burnRate = Util.allOf(((Equipable) from), Attributes.BURN_RATE);
@@ -28,6 +29,27 @@ public class MiscListener {
                 double burn = Util.allOf(((Equipable) from), Attributes.BURN);
                 to.offer(Keys.FIRE_TICKS, to.get(Keys.FIRE_TICKS).orElse(0) + (int) burn);
             }
+        }
+    }
+
+    @Listener
+    public void onLifeSteal(DamageEntityEvent event, @Getter("getTargetEntity") Entity to, @First EntityDamageSource source) {
+        Entity from = source.getSource();
+        if (from instanceof Equipable) {
+            double lifeStealRate = Util.allOf(((Equipable) from), Attributes.LIFE_STEAL_RATE);
+            if (random.nextDouble() < lifeStealRate) {
+                double lifeSteal = Util.allOf(((Equipable) from), Attributes.LIFE_STEAL);
+                to.offer(Keys.HEALTH, to.get(Keys.HEALTH).orElse(0D) + lifeSteal);
+            }
+        }
+    }
+
+    @Listener
+    public void onKnockback(AttackEntityEvent event, @Getter("getTargetEntity") Entity to, @First EntityDamageSource source) {
+        Entity from = source.getSource();
+        if (from instanceof Equipable) {
+            double knockback = Util.allOf(((Equipable) from), Attributes.KNOCKBACK);
+            event.setKnockbackModifier(event.getKnockbackModifier() + (int) knockback);
         }
     }
 }
