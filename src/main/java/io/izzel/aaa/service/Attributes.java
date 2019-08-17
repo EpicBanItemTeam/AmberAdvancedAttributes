0289472fd77389363602e50f1833204dedb48054
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2019-08-18 00:27:55 +0800

    Add Attributes#getAttributeById

--- src/main/java/io/izzel/aaa/service/Attributes.java
+++ src/main/java/io/izzel/aaa/service/Attributes.java
@@ -2,6 +2,7 @@ package io.izzel.aaa.service;
 
 import io.izzel.aaa.data.MarkerValue;
 import io.izzel.aaa.data.RangeValue;
+import org.spongepowered.api.data.DataSerializable;
 import org.spongepowered.api.profile.GameProfile;
 import org.spongepowered.api.text.Text;
 
@@ -47,49 +48,53 @@ public final class Attributes {
 
     public static final Attribute<Text> ORIGINAL_LORE;
 
-    private static RuntimeException error() {
-        return new RuntimeException("The class is loaded too early! ");
+    private static <T extends DataSerializable> Attribute<T> getAttributeById(AttributeService s, String id) {
+        return s.<T>getAttributeById(id).orElseThrow(() -> new RuntimeException("The class is loaded too early! "));
     }
 
     static {
         AttributeService service = AttributeService.instance();
 
-        ATTACK = service.<RangeValue>getAttributeById("aaa-attack").orElseThrow(Attributes::error);
-        POSSESSION = service.<GameProfile>getAttributeById("aaa-possession").orElseThrow(Attributes::error);
-        TRACING = service.<RangeValue>getAttributeById("aaa-tracing").orElseThrow(Attributes::error);
-        PVP_ATTACK = service.<RangeValue>getAttributeById("aaa-pvp-attack").orElseThrow(Attributes::error);
-        PVE_ATTACK = service.<RangeValue>getAttributeById("aaa-pve-attack").orElseThrow(Attributes::error);
-        DEFENSE = service.<RangeValue>getAttributeById("aaa-defense").orElseThrow(Attributes::error);
-        PVP_DEFENSE = service.<RangeValue>getAttributeById("aaa-pvp-defense").orElseThrow(Attributes::error);
-        PVE_DEFENSE = service.<RangeValue>getAttributeById("aaa-pve-defense").orElseThrow(Attributes::error);
-        REFLECT = service.<RangeValue>getAttributeById("aaa-reflect").orElseThrow(Attributes::error);
-        PVP_REFLECT = service.<RangeValue>getAttributeById("aaa-pvp-reflect").orElseThrow(Attributes::error);
-        PVE_REFLECT = service.<RangeValue>getAttributeById("aaa-pve-reflect").orElseThrow(Attributes::error);
-        REFLECT_RATE = service.<RangeValue>getAttributeById("aaa-reflect-rate").orElseThrow(Attributes::error);
-        CRIT = service.<RangeValue>getAttributeById("aaa-critical").orElseThrow(Attributes::error);
-        CRIT_RATE = service.<RangeValue>getAttributeById("aaa-critical-rate").orElseThrow(Attributes::error);
-        DODGE = service.<RangeValue>getAttributeById("aaa-dodge").orElseThrow(Attributes::error);
-        ACCURACY = service.<RangeValue>getAttributeById("aaa-accuracy").orElseThrow(Attributes::error);
-        ACCELERATE = service.<RangeValue>getAttributeById("aaa-accelerate").orElseThrow(Attributes::error);
-        ATTACK_SPEED = service.<RangeValue.Fixed>getAttributeById("aaa-attack-speed").orElseThrow(Attributes::error);
-        MOVE_SPEED = service.<RangeValue.Fixed>getAttributeById("aaa-move-speed").orElseThrow(Attributes::error);
-        DURABILITY = service.<RangeValue>getAttributeById("aaa-durability").orElseThrow(Attributes::error);
-        UNBREAKABLE = service.<MarkerValue>getAttributeById("aaa-unbreakable").orElseThrow(Attributes::error);
-        LOOT_RATE = service.<RangeValue>getAttributeById("aaa-loot-rate").orElseThrow(Attributes::error);
-        LOOT_IMMUNE = service.<MarkerValue>getAttributeById("aaa-loot-immune").orElseThrow(Attributes::error);
-        BURN = service.<RangeValue>getAttributeById("aaa-burn").orElseThrow(Attributes::error);
-        BURN_RATE = service.<RangeValue>getAttributeById("aaa-burn-rate").orElseThrow(Attributes::error);
-        LIFE_STEAL = service.<RangeValue>getAttributeById("aaa-life-steal").orElseThrow(Attributes::error);
-        LIFE_STEAL_RATE = service.<RangeValue>getAttributeById("aaa-life-steal-rate").orElseThrow(Attributes::error);
-        MAX_HEALTH = service.<RangeValue.Fixed>getAttributeById("aaa-max-health").orElseThrow(Attributes::error);
-        ATTACK_RANGE = service.<RangeValue.Fixed>getAttributeById("aaa-attack-range").orElseThrow(Attributes::error);
-        STARVATION = service.<RangeValue>getAttributeById("aaa-starvation").orElseThrow(Attributes::error);
-        SATURATION = service.<RangeValue>getAttributeById("aaa-saturation").orElseThrow(Attributes::error);
-        REGENERATION = service.<RangeValue>getAttributeById("aaa-regeneration").orElseThrow(Attributes::error);
-        KNOCKBACK = service.<RangeValue>getAttributeById("aaa-knockback").orElseThrow(Attributes::error);
-        INSTANT_DEATH = service.<RangeValue>getAttributeById("aaa-instant-death").orElseThrow(Attributes::error);
-        INSTANT_DEATH_IMMUNE = service.<MarkerValue>getAttributeById("aaa-instant-death-immune").orElseThrow(Attributes::error);
-        ORIGINAL_LORE = service.<Text>getAttributeById("aaa-original-lore").orElseThrow(Attributes::error);
+        ATTACK = getAttributeById(service, "aaa-attack");
+        TRACING = getAttributeById(service, "aaa-tracing");
+        PVP_ATTACK = getAttributeById(service, "aaa-pvp-attack");
+        PVE_ATTACK = getAttributeById(service, "aaa-pve-attack");
+        DEFENSE = getAttributeById(service, "aaa-defense");
+        PVP_DEFENSE = getAttributeById(service, "aaa-pvp-defense");
+        PVE_DEFENSE = getAttributeById(service, "aaa-pve-defense");
+        REFLECT = getAttributeById(service, "aaa-reflect");
+        PVP_REFLECT = getAttributeById(service, "aaa-pvp-reflect");
+        PVE_REFLECT = getAttributeById(service, "aaa-pve-reflect");
+        REFLECT_RATE = getAttributeById(service, "aaa-reflect-rate");
+
+        CRIT = getAttributeById(service, "aaa-critical");
+        CRIT_RATE = getAttributeById(service, "aaa-critical-rate");
+        DODGE = getAttributeById(service, "aaa-dodge");
+        ACCURACY = getAttributeById(service, "aaa-accuracy");
+        ACCELERATE = getAttributeById(service, "aaa-accelerate");
+
+        ATTACK_SPEED = getAttributeById(service, "aaa-attack-speed");
+        MOVE_SPEED = getAttributeById(service, "aaa-move-speed");
+        DURABILITY = getAttributeById(service, "aaa-durability");
+        UNBREAKABLE = getAttributeById(service, "aaa-unbreakable");
+        LOOT_RATE = getAttributeById(service, "aaa-loot-rate");
+        LOOT_IMMUNE = getAttributeById(service, "aaa-loot-immune");
+        BURN = getAttributeById(service, "aaa-burn");
+        BURN_RATE = getAttributeById(service, "aaa-burn-rate");
+        LIFE_STEAL = getAttributeById(service, "aaa-life-steal");
+        LIFE_STEAL_RATE = getAttributeById(service, "aaa-life-steal-rate");
+        MAX_HEALTH = getAttributeById(service, "aaa-max-health");
+        ATTACK_RANGE = getAttributeById(service, "aaa-attack-range");
+        STARVATION = getAttributeById(service, "aaa-starvation");
+        SATURATION = getAttributeById(service, "aaa-saturation");
+        REGENERATION = getAttributeById(service, "aaa-regeneration");
+        KNOCKBACK = getAttributeById(service, "aaa-knockback");
+        INSTANT_DEATH = getAttributeById(service, "aaa-instant-death");
+        INSTANT_DEATH_IMMUNE = getAttributeById(service, "aaa-instant-death-immune");
+
+        POSSESSION = getAttributeById(service, "aaa-possession");
+
+        ORIGINAL_LORE = getAttributeById(service, "aaa-original-lore");
     }
 
     private Attributes() {
