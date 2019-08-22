bc76a93fd877fc1a20aeebbcc1f10b31bbd7d1c5
IzzelAliz <csh2001331@126.com>
2019-08-22 17:03:31 +0800

    Very tidy main class

--- src/main/java/io/izzel/aaa/AmberAdvancedAttributes.java
+++ src/main/java/io/izzel/aaa/AmberAdvancedAttributes.java
@@ -1,35 +1,14 @@
 package io.izzel.aaa;
 
 import com.google.inject.Inject;
-import com.google.inject.Injector;
 import io.izzel.aaa.command.AttributeCommands;
-import io.izzel.aaa.listener.ArrowListener;
-import io.izzel.aaa.listener.AttackListener;
-import io.izzel.aaa.listener.MiscListener;
-import io.izzel.aaa.listener.PossessionListener;
 import io.izzel.aaa.service.AttributeService;
-import org.spongepowered.api.event.EventManager;
-import org.spongepowered.api.event.Listener;
-import org.spongepowered.api.event.game.state.GameStartingServerEvent;
 import org.spongepowered.api.plugin.Plugin;
 
 @Plugin(id = "amberadvancedattributes", description = "An AmberAdvancedAttributes item attribute plugin.")
 public class AmberAdvancedAttributes {
 
-    private final EventManager eventManager;
-    private final Injector injector;
-
     @Inject
-    public AmberAdvancedAttributes(AttributeService service, AttributeCommands commands, EventManager e, Injector i) {
-        this.eventManager = e;
-        this.injector = i;
-    }
-
-    @Listener
-    public void on(GameStartingServerEvent event) {
-        this.eventManager.registerListeners(this, this.injector.getInstance(AttackListener.class));
-        this.eventManager.registerListeners(this, this.injector.getInstance(ArrowListener.class));
-        this.eventManager.registerListeners(this, this.injector.getInstance(PossessionListener.class));
-        this.eventManager.registerListeners(this, this.injector.getInstance(MiscListener.class));
+    public AmberAdvancedAttributes(AttributeService service, AttributeCommands commands) {
     }
 }
