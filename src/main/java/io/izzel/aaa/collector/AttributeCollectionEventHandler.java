60e49efa67f61ac8aed52efc2d248b8f615ec0b4
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2019-08-27 17:57:39 +0800

    Implement methods in AttributeCollectionEventHandler

--- src/main/java/io/izzel/aaa/collector/AttributeCollectionEventHandler.java
+++ src/main/java/io/izzel/aaa/collector/AttributeCollectionEventHandler.java
@@ -2,9 +2,22 @@ package io.izzel.aaa.collector;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import io.izzel.aaa.byteitems.ByteItemsHandler;
+import io.izzel.aaa.data.StringValue;
+import io.izzel.aaa.service.Attribute;
+import io.izzel.aaa.service.Attributes;
+import io.izzel.aaa.util.EquipmentUtil;
+import org.slf4j.Logger;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.data.DataSerializable;
+import org.spongepowered.api.entity.Equipable;
 import org.spongepowered.api.event.EventManager;
+import org.spongepowered.api.item.inventory.ItemStackSnapshot;
 import org.spongepowered.api.plugin.PluginContainer;
 import org.spongepowered.api.util.annotation.NonnullByDefault;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 /**
  * @author ustc_zzzz
@@ -12,17 +25,63 @@ import org.spongepowered.api.util.annotation.NonnullByDefault;
 @Singleton
 @NonnullByDefault
 public class AttributeCollectionEventHandler {
+    private final ByteItemsHandler handler;
+    private final Logger logger;
+
     @Inject
-    public AttributeCollectionEventHandler(PluginContainer container, EventManager manager) {
+    public AttributeCollectionEventHandler(PluginContainer container, ByteItemsHandler handler, Logger logger, EventManager manager) {
+        this.logger = logger;
+        this.handler = handler;
         manager.registerListener(container, AttributeCollectionEvent.class, this::onSuit);
         manager.registerListener(container, AttributeCollectionEvent.class, this::onTemplate);
     }
 
     private void onSuit(AttributeCollectionEvent event) {
-        // TODO
+        if (!event.getCause().contains(this.handler)) {
+            if (event.getTargetEntity() instanceof Equipable) {
+                ItemStackSnapshot item = event.getTargetItem();
+                Equipable equipable = (Equipable) event.getTargetEntity();
+                for (StringValue value : Attributes.SUIT.getValues(item)) {
+                    if (EquipmentUtil.instance()
+                            .items(equipable).filter(i -> !i.isEmpty())
+                            .map(Attributes.SUIT::getValues).allMatch(values -> values.contains(value))) {
+                        Sponge.getCauseStackManager().pushCause(this.handler);
+                        AttributeCollector collector = AttributeCollector.of(item);
+                        for (Attribute<?> attribute : event.getCollectedAttributes()) {
+                            collector = this.collect(attribute, event, collector);
+                        }
+                        collector.submit();
+                    }
+                }
+            }
+        }
     }
 
     private void onTemplate(AttributeCollectionEvent event) {
-        // TODO
+        Deque<StringValue> deque = new ArrayDeque<>(Attributes.TEMPLATE.getValues(event.getTargetItem()).reverse());
+        while (!deque.isEmpty()) {
+            String template = deque.removeLast().getString().replace(";", "");
+            ItemStackSnapshot templateItem = this.handler.read(template);
+            if (!templateItem.isEmpty()) {
+                for (Attribute<?> attribute : event.getCollectedAttributes()) {
+                    this.merge(attribute, event, templateItem);
+                }
+                for (StringValue value : Attributes.TEMPLATE.getValues(templateItem).reverse()) {
+                    if (!deque.contains(value)) {
+                        deque.addLast(value);
+                        continue;
+                    }
+                    this.logger.warn("Duplicate template: {} (removed to prevent infinite loops)", value.getString());
+                }
+            }
+        }
+    }
+
+    private <T extends DataSerializable> AttributeCollector collect(Attribute<T> attribute, AttributeCollectionEvent event, AttributeCollector collector) {
+        return collector.collect(attribute, event.getCollectionByAttribute(attribute));
+    }
+
+    private <T extends DataSerializable> void merge(Attribute<T> attribute, AttributeCollectionEvent event, ItemStackSnapshot item) {
+        event.getCollectionByAttribute(attribute).addAll(attribute.getValues(item));
     }
 }
