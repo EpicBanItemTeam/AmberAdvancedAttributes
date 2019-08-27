df8a1f622c6a27e0389689dbdd3e373bbacc55bd
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2019-08-27 11:53:39 +0800

    Add declaration of AttributeCollectionEvent

---
+++ src/main/java/io/izzel/aaa/collector/AttributeCollectionEvent.java
@@ -0,0 +1,31 @@
+package io.izzel.aaa.collector;
+
+import io.izzel.aaa.service.Attribute;
+import org.spongepowered.api.data.DataSerializable;
+import org.spongepowered.api.event.entity.living.TargetLivingEvent;
+import org.spongepowered.api.item.inventory.equipment.EquipmentType;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author ustc_zzzz
+ */
+public interface AttributeCollectionEvent extends TargetLivingEvent {
+    /**
+     * @return target equipment
+     */
+    EquipmentType getTargetEquipment();
+
+    /**
+     * @return an unmodifiable set which contains attributes to be collected
+     */
+    Set<Attribute<? extends DataSerializable>> getCollectedAttributes();
+
+    /**
+     * The default elements are return elements of {@link Attribute#getValues}
+     *
+     * @return a modifiable list which can be used to add values or clear values
+     */
+    <T extends DataSerializable> List<? super T> getCollectionByAttribute(Attribute<T> attribute);
+}
