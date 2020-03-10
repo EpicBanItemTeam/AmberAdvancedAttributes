173fa9b36f81311741868d53be846c2d8e60d152
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2020-03-11 06:20:27 +0800

    Add listenTo method to util

--- src/main/scala/io/izzel/aaa/util/package.scala
+++ src/main/scala/io/izzel/aaa/util/package.scala
@@ -11,6 +11,10 @@ import scala.util.continuations.{shift, suspendable}
 package object util {
   private val handlers: mutable.Map[Class[_ <: Event], mutable.Queue[(Event => Boolean, Event => Unit)]] = mutable.Map()
 
+  private class ScalaEventListener[E <: Event: ClassTag](handler: E => Unit) extends EventListener[E] {
+    override def handle(event: E): Unit = handler(event)
+  }
+
   def waitFor[E <: Event : ClassTag](implicit container: PluginContainer): E@suspendable = waitFor((_: E) => true)
 
   def waitFor[E <: Event : ClassTag](filter: E => Boolean)(implicit container: PluginContainer): E@suspendable = {
@@ -18,12 +22,19 @@ package object util {
       val eventClass = classTag[E].runtimeClass.asInstanceOf[Class[E]]
       val queue: mutable.Queue[(Event => Boolean, Event => Unit)] = handlers.getOrElseUpdate(eventClass, {
         val queue = mutable.Queue[(Event => Boolean, Event => Unit)]()
-        Sponge.getEventManager.registerListener(container, eventClass, new EventListener[E] {
-          override def handle(event: E): Unit = queue.dequeueAll(_._1(event)).foreach(_._2(event))
-        })
+        val handler = (event: E) => queue.dequeueAll(_._1(event)).foreach(_._2(event))
+        Sponge.getEventManager.registerListener(container, eventClass, new ScalaEventListener[E](handler))
         queue
       })
       queue.enqueue((e => filter(eventClass.cast(e)), e => continue(eventClass.cast(e))))
     }
+  }
+
+  def listenTo[E <: Event : ClassTag](handler: E => Unit)(implicit container: PluginContainer): () => Unit = {
+    val eventListener = new ScalaEventListener[E](handler)
+    val eventClass = classTag[E].runtimeClass.asInstanceOf[Class[E]]
+    Sponge.getEventManager.registerListener(container, eventClass, eventListener)
+    val unsubscribeHandler = () => Sponge.getEventManager.unregisterListeners(eventListener)
+    unsubscribeHandler
   }
 }
