b82ab92ddd09a2b6e51ef7d0a68255bade83264d
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2020-03-12 18:15:32 +0800

    Add method to convert TemplateSlot to AttributeTemplate

--- src/api/java/io/izzel/aaa/api/template/TemplateSlot.java
+++ src/api/java/io/izzel/aaa/api/template/TemplateSlot.java
@@ -5,6 +5,8 @@ import org.spongepowered.api.entity.living.player.Player;
 import java.util.List;
 
 public interface TemplateSlot {
+    AttributeTemplate asTemplate();
+
     List<? extends AttributeTemplate> getTemplates(Player player) throws UnreachableSlotException;
 
     void setTemplates(Player player, List<? extends AttributeTemplate> templates) throws UnreachableSlotException;
