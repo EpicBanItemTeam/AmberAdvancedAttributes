0fd3961adde9f775bbc8be128030e2af6a027309
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2020-03-12 16:51:08 +0800

    Add empty and default implementations for visitors

--- src/api/java/io/izzel/aaa/api/visitor/AttributeVisitor.java
+++ src/api/java/io/izzel/aaa/api/visitor/AttributeVisitor.java
@@ -1,5 +1,6 @@
 package io.izzel.aaa.api.visitor;
 
+import com.google.common.base.Preconditions;
 import io.izzel.aaa.api.Attribute;
 import io.izzel.aaa.api.AttributeTemplate;
 
@@ -12,6 +13,29 @@ import io.izzel.aaa.api.AttributeTemplate;
  * - visitEnd
  */
 public interface AttributeVisitor {
+    AttributeVisitor EMPTY = new AttributeVisitor() {
+        @Override
+        public <T> void visit(Attribute<T> attribute, T data) {
+            Preconditions.checkNotNull(attribute);
+            Preconditions.checkNotNull(data);
+        }
+
+        @Override
+        public Templates visitTemplates() {
+            return Templates.EMPTY;
+        }
+
+        @Override
+        public void visitEnd() {
+            // do nothing here
+        }
+
+        @Override
+        public String toString() {
+            return AttributeVisitor.class.getName() + ".EMPTY";
+        }
+    };
+
     <T> void visit(Attribute<T> attribute, T data);
 
     Templates visitTemplates();
@@ -19,6 +43,24 @@ public interface AttributeVisitor {
     void visitEnd();
 
     interface Templates {
+        Templates EMPTY = new Templates() {
+            @Override
+            public AttributeVisitor visitTemplate(AttributeTemplate template) {
+                Preconditions.checkNotNull(template);
+                return AttributeVisitor.EMPTY;
+            }
+
+            @Override
+            public void visitTemplateEnd() {
+                // do nothing here
+            }
+
+            @Override
+            public String toString() {
+                return Templates.class.getName() + ".EMPTY";
+            }
+        };
+
         AttributeVisitor visitTemplate(AttributeTemplate template);
 
         void visitTemplateEnd();
