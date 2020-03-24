c84b26d2558491a7490f91849c89f449d9c1a248
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2020-03-24 22:16:40 +0800

    Add type serializer for range value

--- src/main/scala/io/izzel/aaa/range/RangeParser.scala
+++ src/main/scala/io/izzel/aaa/range/RangeParser.scala
@@ -1,8 +1,22 @@
 package io.izzel.aaa.range
 
+import com.google.common.reflect.TypeToken
+import ninja.leaping.configurate.ConfigurationNode
+import ninja.leaping.configurate.objectmapping.ObjectMappingException
+import ninja.leaping.configurate.objectmapping.serialize.{TypeSerializer, TypeSerializers}
+
 import scala.util.parsing.combinator._
 
 object RangeParser extends RegexParsers {
+
+  TypeSerializers.getDefaultSerializers.registerType(TypeToken.of(classOf[Value]), new TypeSerializer[Value] {
+    override def serialize(t: TypeToken[_], o: Value, v: ConfigurationNode): Unit = v.setValue(o.toString)
+
+    override def deserialize(t: TypeToken[_], v: ConfigurationNode): Value = parseAll(range, v.getString("")) match {
+      case Failure(msg, _) => throw new ObjectMappingException(msg)
+      case Success(result, _) => result
+    }
+  })
 
   case class DoubleWithPercent(value: Double, percent: Double) {
     override def toString: String = f"$value%+f${"-++".charAt(percent.signum + 1)}${percent.abs}%f%%"
@@ -19,39 +33,39 @@ object RangeParser extends RegexParsers {
   private def percent: Parser[DoubleWithPercent] = (num <~ "%").^^(DoubleWithPercent(0, _))
 
   private def numProduct: Parser[Double] = (numValue ~ rep(ws ~> ("*" | "/") ~ (ws ~> numValue))).^^ {
-    case value ~ tail => tail.foldLeft(value)((a, b) => b match {
-      case "*" ~ c => a * c
-      case "/" ~ c => a / c
-    })
+    case value ~ tail => tail.foldLeft(value) {
+      case (a, "*" ~ c) => a * c
+      case (a, "/" ~ c) => a / c
+    }
   }
 
   private def numSum: Parser[Double] = (numProduct ~ rep(ws ~> ("+" | "-") ~ (ws ~> numProduct))).^^ {
-    case value ~ tail => tail.foldLeft(value)((a, b) => b match {
-      case "+" ~ c => a + c
-      case "-" ~ c => a - c
-    })
+    case value ~ tail => tail.foldLeft(value) {
+      case (a, "+" ~ c) => a + c
+      case (a, "-" ~ c) => a - c
+    }
   }
 
   private def numValue: Parser[Double] = "(" ~> numSum <~ ")" | num
 
   private def productReverse: Parser[DoubleWithPercent] = (rep(numValue ~ (ws ~> "*")) ~ (ws ~> value)).^^ {
-    case head ~ value => head.foldRight(value)((a, b) => a match {
-      case c ~ "*" => DoubleWithPercent(c * b.value, c * b.percent)
-    })
+    case head ~ value => head.foldRight(value) {
+      case (c ~ "*", b) => DoubleWithPercent(c * b.value, c * b.percent)
+    }
   }
 
   private def product: Parser[DoubleWithPercent] = (productReverse ~ rep(ws ~> ("*" | "/") ~ (ws ~> numValue))).^^ {
-    case value ~ tail => tail.foldLeft(value)((a, b) => b match {
-      case "*" ~ c => DoubleWithPercent(a.value * c, a.percent * c)
-      case "/" ~ c => DoubleWithPercent(a.value / c, a.percent / c)
-    })
+    case value ~ tail => tail.foldLeft(value) {
+      case (a, "*" ~ c) => DoubleWithPercent(a.value * c, a.percent * c)
+      case (a, "/" ~ c) => DoubleWithPercent(a.value / c, a.percent / c)
+    }
   }
 
   private def sum: Parser[DoubleWithPercent] = (product ~ rep(ws ~> ("+" | "-") ~ (ws ~> product))).^^ {
-    case value ~ tail => tail.foldLeft(value)((a, b) => b match {
-      case "+" ~ c => DoubleWithPercent(a.value + c.value, a.percent + c.percent)
-      case "-" ~ c => DoubleWithPercent(a.value - c.value, a.percent - c.percent)
-    })
+    case value ~ tail => tail.foldLeft(value) {
+      case (a, "+" ~ c) => DoubleWithPercent(a.value + c.value, a.percent + c.percent)
+      case (a, "-" ~ c) => DoubleWithPercent(a.value - c.value, a.percent - c.percent)
+    }
   }
 
   private def value: Parser[DoubleWithPercent] = "(" ~> sum <~ ")" | percent | numValue.^^(DoubleWithPercent(_, 0))
