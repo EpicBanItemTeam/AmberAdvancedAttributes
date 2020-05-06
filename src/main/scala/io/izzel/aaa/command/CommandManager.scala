5772d3f8ccb4c83a41ecfdeba0872a20657b554e
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2020-05-06 14:18:56 +0800

    Make commands work only on main hand slots or global slots

--- src/main/scala/io/izzel/aaa/command/CommandManager.scala
+++ src/main/scala/io/izzel/aaa/command/CommandManager.scala
@@ -4,7 +4,7 @@ import java.util.{UUID, function}
 
 import com.google.inject.{Inject, Singleton}
 import io.izzel.aaa
-import io.izzel.aaa.api.data.{Template, TemplateSlot, UnreachableSlotException}
+import io.izzel.aaa.api.data.{Template, TemplateSlots, UnreachableSlotException}
 import io.izzel.aaa.attribute.AttributeManager
 import io.izzel.aaa.config.ConfigManager
 import io.izzel.aaa.data.CustomTemplates
@@ -16,7 +16,7 @@ import org.spongepowered.api.Sponge
 import org.spongepowered.api.command.args.CommandContext
 import org.spongepowered.api.command.spec.{CommandExecutor, CommandSpec}
 import org.spongepowered.api.command.{CommandResult, CommandSource}
-import org.spongepowered.api.data.`type`.HandType
+import org.spongepowered.api.data.`type`.HandTypes
 import org.spongepowered.api.data.key.Keys
 import org.spongepowered.api.data.manipulator.DataManipulator
 import org.spongepowered.api.entity.living.player.Player
@@ -39,15 +39,8 @@ class CommandManager @Inject()(implicit container: PluginContainer, config: Conf
     })
   }
 
-  private val slotKey: Text = Text.of("slot")
   private val templateKey: Text = Text.of("template")
-  private val slotArg: function.Supplier[java.util.Collection[String]] with function.Function[String, TemplateSlot] = {
-    new function.Supplier[java.util.Collection[String]] with function.Function[String, TemplateSlot] {
-      override def get: java.util.Collection[String] = manager.slotMap.keys.map(_.toString).toList.asJava
 
-      override def apply(key: String): TemplateSlot = Template.tryParse(key).asScala.flatMap(manager.slotMap.get).orNull
-    }
-  }
   private val templateArg: function.Supplier[java.util.Collection[String]] with function.Function[String, Template] = {
     new function.Supplier[java.util.Collection[String]] with function.Function[String, Template] {
       override def get: java.util.Collection[String] = config.keys.map(_.toString).toSet.asJava
@@ -57,60 +50,50 @@ class CommandManager @Inject()(implicit container: PluginContainer, config: Conf
   }
 
   private def init(src: CommandSource, args: CommandContext): Unit = src match {
-    case player: Player => locally {
-      val handTypes = Sponge.getRegistry.getAllOf(classOf[HandType]).asScala
-      handTypes.find(player.getItemInHand(_).isPresent) match {
-        case Some(handType) => locally {
-          val item = player.getItemInHand(handType).get
-          item.get(classOf[CustomTemplates.Data]).asScala match {
-            case None => locally {
-              val uuid = new UUID(0, 0)
-              val templateData = CustomTemplates.Builder.create()
-              val displayName = item.get[Text](Keys.DISPLAY_NAME).asScala
-              val lore = item.get[java.util.List[Text]](Keys.ITEM_LORE).asScala.map(_.asScala.toList)
+    case player: Player => player.getItemInHand(HandTypes.MAIN_HAND).asScala match {
+      case Some(item) => item.get(classOf[CustomTemplates.Data]).asScala match {
+        case None => locally {
+          val uuid = new UUID(0, 0)
+          val templateData = CustomTemplates.Builder.create()
+          val displayName = item.get[Text](Keys.DISPLAY_NAME).asScala
+          val lore = item.get[java.util.List[Text]](Keys.ITEM_LORE).asScala.map(_.asScala.toList)
 
-              templateData.value = CustomTemplates.Value(uuid, CustomTemplates.Backup(displayName, lore), Nil)
-              item.offer(templateData.asInstanceOf[DataManipulator[_, _]])
-              item.remove(Keys.DISPLAY_NAME)
-              item.remove(Keys.ITEM_LORE)
+          templateData.value = CustomTemplates.Value(uuid, CustomTemplates.Backup(displayName, lore), Nil)
+          item.offer(templateData.asInstanceOf[DataManipulator[_, _]])
+          item.remove(Keys.DISPLAY_NAME)
+          item.remove(Keys.ITEM_LORE)
 
-              src.asInstanceOf[Player].setItemInHand(handType, item)
-              locale.to(src, "commands.init.succeed")
-            }
-            case Some(_) => locale.to(src, "commands.init.already-exist")
-          }
+          src.asInstanceOf[Player].setItemInHand(HandTypes.MAIN_HAND, item)
+          locale.to(src, "commands.init.succeed")
         }
-        case None => locale.to(src, "commands.init.nonexist")
+        case Some(_) => locale.to(src, "commands.init.already-exist")
       }
+      case None => locale.to(src, "commands.init.nonexist")
     }
     case _ => locale.to(src, "commands.init.nonexist")
   }
 
   private def drop(src: CommandSource, args: CommandContext): Unit = src match {
-    case player: Player => locally {
-      var isCallbackExecuted = false
-      val handTypes = Sponge.getRegistry.getAllOf(classOf[HandType]).asScala
-      handTypes.find(player.getItemInHand(_).isPresent) match {
+    case player: Player => player.getItemInHand(HandTypes.MAIN_HAND).asScala match {
+      case Some(item) => item.get(classOf[CustomTemplates.Data]).asScala match {
         case Some(_) => locally {
+          var isCallbackExecuted = false
           val arg = Arg.ref("commands.drop.warning-ok").withCallback(new function.Consumer[CommandSource] {
             override def accept(t: CommandSource): Unit = if (!isCallbackExecuted) {
-              handTypes.find(player.getItemInHand(_).isPresent) match {
-                case Some(handType) => locally {
-                  val item = player.getItemInHand(handType).get
-                  item.get(classOf[CustomTemplates.Data]).asScala match {
-                    case Some(data) => locally {
-                      isCallbackExecuted = true
+              player.getItemInHand(HandTypes.MAIN_HAND).asScala match {
+                case Some(item) => item.get(classOf[CustomTemplates.Data]).asScala match {
+                  case Some(data) => locally {
+                    isCallbackExecuted = true
 
-                      val CustomTemplates.Backup(name, lore) = data.value.backup
-                      name.map(value => item.offer(Keys.DISPLAY_NAME, value)).getOrElse(item.remove(Keys.DISPLAY_NAME))
-                      lore.map(value => item.offer(Keys.ITEM_LORE, value.asJava)).getOrElse(item.remove(Keys.ITEM_LORE))
-                      item.remove(classOf[CustomTemplates.Data])
+                    val CustomTemplates.Backup(name, lore) = data.value.backup
+                    name.map(value => item.offer(Keys.DISPLAY_NAME, value)).getOrElse(item.remove(Keys.DISPLAY_NAME))
+                    lore.map(value => item.offer(Keys.ITEM_LORE, value.asJava)).getOrElse(item.remove(Keys.ITEM_LORE))
+                    item.remove(classOf[CustomTemplates.Data])
 
-                      src.asInstanceOf[Player].setItemInHand(handType, item)
-                      locale.to(src, "commands.drop.succeed")
-                    }
-                    case None => locale.to(src, "commands.drop.nonexist")
+                    src.asInstanceOf[Player].setItemInHand(HandTypes.MAIN_HAND, item)
+                    locale.to(src, "commands.drop.succeed")
                   }
+                  case None => locale.to(src, "commands.drop.nonexist")
                 }
                 case None => locale.to(src, "commands.drop.nonexist")
               }
@@ -120,13 +103,14 @@ class CommandManager @Inject()(implicit container: PluginContainer, config: Conf
         }
         case None => locale.to(src, "commands.drop.nonexist")
       }
+      case None => locale.to(src, "commands.drop.nonexist")
     }
     case _ => locale.to(src, "commands.drop.nonexist")
   }
 
   private def show(src: CommandSource, args: CommandContext): Unit = src match {
     case player: Player => try {
-      val slot = args.getOne[TemplateSlot](slotKey).get
+      val slot = if (args.hasAny("global")) TemplateSlots.GLOBAL else TemplateSlots.MAIN_HAND
       val templates = slot.getTemplates(player).asScala.toSet[Template]
       if (templates.isEmpty) locale.to(src, "commands.show.absent") else {
         locale.to(src, "commands.show.present", Int.box(templates.size))
@@ -143,7 +127,7 @@ class CommandManager @Inject()(implicit container: PluginContainer, config: Conf
 
   private def apply(src: CommandSource, args: CommandContext): Unit = src match {
     case player: Player => try {
-      val slot = args.getOne[TemplateSlot](slotKey).get
+      val slot = if (args.hasAny("global")) TemplateSlots.GLOBAL else TemplateSlots.MAIN_HAND
       val templates = slot.getTemplates(player).asScala.toSet[Template]
       val templatesToAdd = args.getAll[Template](templateKey).asScala.toSet.diff(templates)
       if (templatesToAdd.isEmpty) locale.to(src, "commands.apply.absent") else {
@@ -162,7 +146,7 @@ class CommandManager @Inject()(implicit container: PluginContainer, config: Conf
 
   private def unapply(src: CommandSource, args: CommandContext): Unit = src match {
     case player: Player => try {
-      val slot = args.getOne[TemplateSlot](slotKey).get
+      val slot = if (args.hasAny("global")) TemplateSlots.GLOBAL else TemplateSlots.MAIN_HAND
       val templates = slot.getTemplates(player).asScala.toSet[Template]
       val templatesToAdd = args.getAll[Template](templateKey).asScala.toSet.intersect(templates)
       if (templatesToAdd.isEmpty) locale.to(src, "commands.unapply.absent") else {
@@ -197,13 +181,13 @@ class CommandManager @Inject()(implicit container: PluginContainer, config: Conf
         .arguments(none())
         .executor(drop _).permission(s"${aaa.id}.command.drop").build(), "drop", "d")
       .child(CommandSpec.builder
-        .arguments(choices(slotKey, slotArg, slotArg, true))
+        .arguments(flags().flag("-global").buildWith(none()))
         .executor(show _).permission(s"${aaa.id}.command.show").build(), "show", "s")
       .child(CommandSpec.builder
-        .arguments(choices(slotKey, slotArg, slotArg, true), allOf(choices(templateKey, templateArg, templateArg, false)))
+        .arguments(flags().flag("-global").buildWith(allOf(choices(templateKey, templateArg, templateArg, false))))
         .executor(apply _).permission(s"${aaa.id}.command.apply").build(), "apply", "a")
       .child(CommandSpec.builder
-        .arguments(choices(slotKey, slotArg, slotArg, true), allOf(choices(templateKey, templateArg, templateArg, false)))
+        .arguments(flags().flag("-global").buildWith(allOf(choices(templateKey, templateArg, templateArg, false))))
         .executor(unapply _).permission(s"${aaa.id}.command.unapply").build(), "unapply", "u")
       .executor(fallback _).build(), "aaa", aaa.id)
     ()
